043a13c91c1c73ebf3f7be120b8aacfb16a4274a
huihoang <hoang.nguyen2211093@hcmut.edu.vn>
2025-11-02 21:50:33 +0700

    fix conflict

--- be/src/main/java/com/teamup/main/controller/AuthController.java
+++ be/src/main/java/com/teamup/main/controller/AuthController.java
@@ -10,7 +10,6 @@ import com.teamup.main.service.AuthService;
 
 import java.io.IOException;
 
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
