eb27628b5999e4b4215c83fdaab58b6f0dc7c2d4
huihoang <hoang.nguyen2211093@hcmut.edu.vn>
2026-01-13 21:08:50 +0700

    setup eas to build

--- fe/app.config.js
+++ fe/app.config.js
@@ -1,5 +1,3 @@
-
-
 module.exports = {
   expo: {
     name: "Team up",
@@ -8,34 +6,42 @@ module.exports = {
     version: "1.0.0",
     orientation: "portrait",
     icon: "./assets/icon.png",
+
     plugins: [
       "expo-secure-store",
       "expo-web-browser"
     ],
+
     splash: {
       image: "./assets/splash-icon.png",
       resizeMode: "contain",
       backgroundColor: "#ffffff"
     },
+
     extra: {
-      // API_BASE_URL: process.env.API_BASE_URL || "https://teamup-mobile-app.onrender.com", // Deployed backend (slow cold start)
-      API_BASE_URL: process.env.API_BASE_URL || "http://localhost:8080", // Local backend
-      GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "67346913521-0bql06om6o8kj610ferhl52le2uqh3jr.apps.googleusercontent.com",
-      GOOGLE_REDIRECT_URI: process.env.GOOGLE_REDIRECT_URI || "http://localhost:8080/auth/login",
+      eas: {
+        projectId: "e1a02a9b-e96e-4979-8f4d-0c5a58edbc6d"
+      },
+      API_BASE_URL: process.env.API_BASE_URL || "http://localhost:8080",
+      GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+      GOOGLE_REDIRECT_URI: process.env.GOOGLE_REDIRECT_URI,
       SKIP_AUTH: false
     },
-    ios: {
-      supportsTablet: true
-    },
+
     android: {
+      package: "com.huihoang.teamup",  // ❗❗ BẮT BUỘC
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#ffffff"
       }
     },
+
+    ios: {
+      supportsTablet: true
+    },
+
     web: {
       bundler: "metro"
     }
   }
 };
-
